a393709b40ad67b9dfff440a5d96de1887dfadf2
Yusuf Jolaoso <jolaosoyusuf@gmail.com>
2021-06-14 16:18:09 +0100

    refactored the status and metric logics

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -13,15 +13,27 @@ def status():
     status = {
         'result': 'OK - healthy'
     }
-    return json.dumps(status)
+    response = app.response_class(
+        response=json.dumps(status),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 
 @app.route("/metrics")
 def metrics():
     metrics = {
+        'success': 'status',
+        'code': 0,
         'data': {'UserCount': 140, 'UserCountActive': 23}
     }
-    return json.dumps(metrics)
+    response = app.response_class(
+        response=json.dumps(metrics),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 
 if __name__ == "__main__":
