39a8ecd9ac4d5640a9ce1645fa004e2c075ffe18
Yusuf Jolaoso <jolaosoyusuf@gmail.com>
2021-06-14 16:04:09 +0100

    added the status endpoint

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,20 @@
+import json
 from flask import Flask
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route("/status")
+def status():
+    status = {
+        'result': 'OK - healthy'
+    }
+    return json.dumps(status)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
